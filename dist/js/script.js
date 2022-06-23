911bb9ab48b7cf76626702b2194dbd6d3ead2400
Danil <dantereshenko69@gmail.com>
2022-06-23 19:39:13 +0600

    feat: finish layouts

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,1 +1,22 @@
+import Swiper from 'https://unpkg.com/swiper@8/swiper-bundle.esm.browser.min.js';
 
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction : 'horizontal',
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 10,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      bulletClass : 'swiper-pagination-bullet',
+      clickable : true,
+      currentClass :'swiper-pagination-current',
+    },
+  
+ 
+  
+ 
+  });
