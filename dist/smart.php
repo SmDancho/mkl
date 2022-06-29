df280ac31968bf677bca50eebe2b54d8281b11e6
Danil <75630533+SmDancho@users.noreply.github.com>
2022-06-29 14:28:17 +0600

    Add files via upload

--- dist/smart.php
+++ dist/smart.php
@@ -21,7 +21,7 @@ $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, 
 $mail->Port = 465;                                    // TCP port to connect to
  
 $mail->setFrom('dantereshenko69@gmail.com', 'MKL');   // От кого письмо 
-$mail->addAddress('hokkey.5@mail.ru');     // Add a recipient
+$mail->addAddress('Kirya060@gmail.com');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
